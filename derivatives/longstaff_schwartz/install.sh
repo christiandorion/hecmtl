518dbfe12e4d420325f8c0e1504b1a9dee5e04cc
christiandorion <christian@svarog.local>
2020-03-21 18:32:48 -0400

    longstaff_schwartz as sent to 60206 students

---
+++ derivatives/longstaff_schwartz/install.sh
@@ -0,0 +1,10 @@
+pip install longstaff-schwartz
+
+% First familiarize with the algo as portrayed in the notebook from_luphord_modified.ipynb
+
+% In a python interpreter:
+%
+% import longstaff_schwartz as ls
+% ls.__file__ # Will allow you to see where the python files were installed
+% # Open the file in read-only mode; read and understand each steps
+% 
